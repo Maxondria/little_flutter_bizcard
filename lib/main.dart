0c0e1702aa34793439a353fcb58d9abd06691062
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-10 21:36:05 +0300

    add and style phone and email widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,34 +10,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100.0,
-                padding: EdgeInsets.all(20.0),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/images/max.jpeg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                  )
-                ],
+              Text(
+                'Max Tayebwa',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                    color: Colors.teal[100],
+                    fontSize: 20.0,
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
               ),
               Container(
-                width: 100.0,
-                color: Colors.blue,
-                padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+256 784 431369',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'maxtayebwa@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
